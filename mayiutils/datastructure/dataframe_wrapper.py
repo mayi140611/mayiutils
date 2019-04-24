f34853066cc2983f3ce834f55f688b43f83b78dd
ian <472156380@qq.com>
2019-04-24 14:44:24 +0800

    dataframe_wrapper

--- mayiutils/datastructure/dataframe_wrapper.py
+++ mayiutils/datastructure/dataframe_wrapper.py
@@ -189,9 +189,154 @@ class DataframeWrapper(object):
 
 
 if __name__ == '__main__':
-    mode = 0
+    """
+    http://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html
+    """
+    mode = 4
+    """
+    DF Creation
+    """
+    # 创建时间序列索引
+    # dates = pd.date_range('20130101', periods=6)
+    # print(dates)
+    """
+DatetimeIndex(['2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04',
+               '2013-01-05', '2013-01-06'],
+              dtype='datetime64[ns]', freq='D')
+    """
+    """
+    load data
+    """
+    # load带有日期列的数据
+    loans = pd.read_csv('../../tmp/loans.csv', parse_dates=['loan_start', 'loan_end'])
+    # print(loans.head())
+    """
+   client_id loan_type  loan_amount  ...  loan_start   loan_end  rate
+0      46109      home        13672  ...  2002-04-16 2003-12-20  2.15
+1      46109    credit         9794  ...  2003-10-21 2005-07-17  1.25
+2      46109      home        12734  ...  2006-02-01 2007-07-05  0.68
+3      46109      cash        12518  ...  2010-12-08 2013-05-05  1.24
+4      46109    credit        14049  ...  2010-07-07 2012-05-21  3.13
+    """
+    d1 = loans['loan_end'].dt
+    # print(d1)#<pandas.core.indexes.accessors.DatetimeProperties object at 0x11639b518>
+    # 获取month or year
+    m = d1.month
+    # print(type(m))#<class 'pandas.core.series.Series'>
+
+    if mode == 1:
+        """
+        查看数据 
+        Viewing Data
+        
+        head(),  默认前5个样本
+        sample(): a random sample of items from an axis of object
+        tail()：默认后5个sample
+        
+        info()
+        describe()#shows a quick statistic summary of your data。注意，df会自动选择数值列进行统计
+        
+        df.T 转置
+        """
+        # loans.info()
+        """
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 443 entries, 0 to 442
+Data columns (total 8 columns):
+client_id      443 non-null int64
+loan_type      443 non-null object
+loan_amount    443 non-null int64
+repaid         443 non-null int64
+loan_id        443 non-null int64
+loan_start     443 non-null datetime64[ns]
+loan_end       443 non-null datetime64[ns]
+rate           443 non-null float64
+dtypes: datetime64[ns](2), float64(1), int64(4), object(1)
+memory usage: 27.8+ KB
+        """
+        # print(loans.describe())
+        """
+          client_id   loan_amount      repaid       loan_id        rate
+count    443.000000    443.000000  443.000000    443.000000  443.000000
+mean   38911.060948   7982.311512    0.534989  11017.101580    3.217156
+std     7768.681063   4172.891992    0.499338    581.826222    2.397168
+min    25707.000000    559.000000    0.000000  10009.000000    0.010000
+25%    32885.000000   4232.500000    0.000000  10507.500000    1.220000
+50%    39505.000000   8320.000000    1.000000  11033.000000    2.780000
+75%    46109.000000  11739.000000    1.000000  11526.000000    4.750000
+max    49624.000000  14971.000000    1.000000  11991.000000   12.620000
+        """
+        # print(loans.T)
+        """
+                             0    ...                  442
+client_id                  46109  ...                26945
+loan_type                   home  ...                 home
+loan_amount                13672  ...                 3643
+repaid                         0  ...                    0
+loan_id                    10243  ...                11434
+loan_start   2002-04-16 00:00:00  ...  2010-03-24 00:00:00
+loan_end     2003-12-20 00:00:00  ...  2011-12-22 00:00:00
+rate                        2.15  ...                 0.13
+        """
+        # print(loans.sort_index(ascending=False))#按照索引降序查看
+        print(loans.sort_values(by='rate'))#按照rate列升序查看
+    if mode == 2:
+        """
+        行列操作
+        """
+        pass
+    if mode == 3:
+        """
+        合并
+        """
+        pass
+    if mode == 4:
+        """
+        groupby
+        followed by a suitable aggregation function
+            可以通过agg函数，但是不太灵活
+            mean()
+            max()
+            min()
+            quantile(0.9)#求每个分组的0.9分位数
+        """
+        # 同时使用多个聚合函数
+        stats = loans.groupby(by='client_id')['loan_amount'].agg(['mean', 'max', 'min'])
+        t = loans.groupby('client_id')
+        # print(t)#<pandas.core.groupby.generic.DataFrameGroupBy object at 0x1a200fbbe0>
+        stats.columns = ['mean_loan_amount', 'max_loan_amount', 'min_loan_amount']
+        # print(type(stats))#<class 'pandas.core.frame.DataFrame'>
+        # print(stats.head())
+        """
+           mean_loan_amount  max_loan_amount  min_loan_amount
+client_id                                                    
+25707           7963.950000            13913             1212
+26326           7270.062500            13464             1164
+26695           7824.722222            14865             2389
+26945           7125.933333            14593              653
+29841           9813.000000            14837             2778
+        """
+        tt = t['loan_amount'].quantile(0.9)
+        # print(type(tt))#<class 'pandas.core.series.Series'>
+
+        # 使用自定义函数进行聚合
+        def peak_to_peak(arr):
+            return arr.max() - arr.min()
+        # print(t['loan_amount'].agg(peak_to_peak))
+        # 查看每个分组的统计摘要信息
+        # print(t['loan_amount'].describe())
+        """
+           count          mean          std  ...      50%       75%      max
+client_id                                    ...                            
+25707       20.0   7963.950000  4149.486062  ...   9086.0  11419.00  13913.0
+26326       16.0   7270.062500  4393.666631  ...   6133.0  11270.75  13464.0
+26695       18.0   7824.722222  4196.462499  ...   9084.5  10289.00  14865.0
+26945       15.0   7125.933333  4543.621769  ...   8337.0   9289.00  14593.0
+
+        """
     # 删除全部为null的列，如果只要出现null就删掉，则how=any
     # df2 = df1.dropna(axis=1, how='all')
+
     if mode == 5:
         data = {'state': [1, 1, 2, 2, 1, 2, 2], 'pop': ['a', 'b', 'c', 'd', 'b', 'c', 'd']}
         frame = pd.DataFrame(data)
@@ -205,7 +350,7 @@ if __name__ == '__main__':
         #保留某一特征中重复值的第一条
         a = frame.drop_duplicates(subset=['pop'], keep='first')
         print(a)
-    if mode == 4:
+    if mode == 4444:
         d = pd.DataFrame([[0, 0], [0, 1], [1, 1]])
         print(d == 1)#判断d中每一个元素是否为1
         """
@@ -265,7 +410,7 @@ if __name__ == '__main__':
         """
         # data 为 None
         print(pd.DataFrame(index=['support', 'confidence']))
-    if mode == 2:
+    if mode == 2222:
         """
         apply
         Apply a function along an axis of the DataFrame.
@@ -296,7 +441,7 @@ if __name__ == '__main__':
         B    27       
         """
         print(df.applymap(np.sqrt))
-    if mode == 1:
+    if mode == 1111:
         df =  pd.read_excel("疾病列表190126.xlsx")
         #获取满足要求的样本
         df1 = DataframeWrapper.isIn(df, '疾病名称（39）', ['嗜睡症', '脑萎缩'])
