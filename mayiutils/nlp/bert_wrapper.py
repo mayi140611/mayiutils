e7f1ce3f8da45b6ffad65ccbfaee84d363fc75b0
ian <yongguiluo@hotmail.com>
2019-10-28 19:10:20 +0800

    sm

--- mayiutils/nlp/bert_wrapper.py
+++ mayiutils/nlp/bert_wrapper.py
@@ -6,26 +6,10 @@
 @file: bert_wrapper.py
 @time: 2019-04-26 14:23
 
-https://blog.csdn.net/weixin_37947156/article/details/84877254
 
-BERT本质上是一个两段式的NLP模型。第一个阶段叫做：Pre-training，跟WordEmbedding类似，利用现有无标记的语料训练一个语言模型。
-第二个阶段叫做：Fine-tuning，利用预训练好的语言模型，完成具体的NLP下游任务。
 
-Google已经投入了大规模的语料和昂贵的机器帮我们完成了Pre-training过程，结果可以在其GitHub上下载
 
-这里主要介绍fine-tuning过程。
 
-回到Github中的代码，只有run_classifier.py和run_squad.py是用来做fine-tuning 的，其他可以暂时不考虑。
-这里使用run_classifier.py进行文本相似度（本质分类建模）。
-
-代码解析
-
-    从主函数开始，可以发现它指定了必须的参数：
-        data_dir指的是我们的输入数据的文件夹路径。查看代码，不难发现，作者给出了输入数据的格式：
-        可以发现它要求的输入分别是guid, text_a, text_b, label，其中text_b和label为可选参数。
-        例如我们要做的是单个句子的分类任务，那么就不需要输入text_b；另外，在test样本中，我们便不需要输入label。
-
-    这里的task_name，一开始可能不好理解它是用来做什么的。仔细查看代码可以发现：task_name是用来选择processor的。
 """
 from mayiutils.nlp.bert.extract_feature import BertVector
 from mayiutils.nlp.bert.similarity import BertSim
